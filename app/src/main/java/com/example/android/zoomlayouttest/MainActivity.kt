70ce3c169ea3dd258a267221581ff39dc09e8212
David Stephen Barry <davidstephenbarry@gmail.com>
2018-09-20 09:56:26 -0700

    Fixed 'push button to zoom' functionality for sq000 square
    Bugs:
     -member functions of zoom objects were broken/unusable after previous commit for a time, but the problem stopped. Unable to reproduce.

--- app/src/main/java/com/example/android/zoomlayouttest/MainActivity.kt
+++ app/src/main/java/com/example/android/zoomlayouttest/MainActivity.kt
@@ -5,10 +5,17 @@ Added basic ZoomLayout functionality
  - Small representative grid of 13 squares can be 'pinch-zoomed' and scrolled in center of screen
  - Each square can be clicked, activating a button listener function, and showing a 'Toast' message on screen
 
-September 18th, 2018
+September 18th, 2018 -- David Barry
 Added function for creating a list of known squares (called createSquareList)
  - Function takes an int representing total # of squares on grid and a blank, mutable list to hold square names
 
+Sept 19th, 2018 -- James Gillis
+Added constraints for zoomLayout view
+Added id tags for top and bottom menu bar
+Fixed constraints for top and bottom menu bar
+
+Sept. 20th, 2018 -- David Barry
+Added test functionality (push button to zoom) for top button in garden grid area
 
  */
 
@@ -18,6 +25,7 @@ package com.example.android.zoomlayouttest
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import com.otaliastudios.zoom.ZoomLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -29,17 +37,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val gardenBedView = findViewById<ZoomLayout>(R.id.zoomLayout)  // zoom
+
+        /* Test Code */
 
         createSquareList(13,squareList)
-      //  createSquareListeners(squareList)
-        //sq000.setOnClickListener{ println(squareList)}
 
+
+        //  createSquareListeners(squareList)
+        //sq000.setOnClickListener{ println(squareList)}
 
         /* Square listeners, output a simple Toast text response. Will be made in code with for loop eventually */
 
 
 
-        sq000.setOnClickListener{ Toast.makeText(this@MainActivity,"Square 0'", Toast.LENGTH_SHORT).show() }
+        sq000.setOnClickListener{ zoomCustom(gardenBedView,2.0f,true) }
         sq001.setOnClickListener{ Toast.makeText(this@MainActivity,"Square 1'", Toast.LENGTH_SHORT).show() }
         sq002.setOnClickListener{ Toast.makeText(this@MainActivity,"Square 2'", Toast.LENGTH_SHORT).show() }
         sq003.setOnClickListener{ Toast.makeText(this@MainActivity,"Square 3'", Toast.LENGTH_SHORT).show() }
@@ -66,7 +78,7 @@ fun createSquareList(numberOfSquares: Int,squareList: MutableList<String>)
 
     for(i in 0..(numberOfSquares-1)){
 
-       if(numberOfSquares < 10)
+        if(numberOfSquares < 10)
         {
             var tempString: String = "sq00" + i.toString()
             squareList.add(tempString)
@@ -94,3 +106,7 @@ fun createSquareListeners(listOfSquares: MutableList<String>)
     }
 }
 
+fun zoomCustom(viewToZoom : ZoomLayout, amountToZoom : Float, animateZoom : Boolean )
+{
+    viewToZoom.zoomTo(amountToZoom,animateZoom)
+}
