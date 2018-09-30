41b71458eb8a0ff1eaeef0c8ce1c8b681f19360b
0ldmanB <davidstephenbarry@gmail.com>
2018-09-30 13:22:35 -0700

    Added ColorData class, used to store colors for tiles Added Square class member function changeColor, used to access and change a square's color Refactored build function

--- app/src/main/java/com/example/android/zoomlayouttest/ColorData.kt
+++ app/src/main/java/com/example/android/zoomlayouttest/ColorData.kt
@@ -7,5 +7,6 @@ class ColorData{
         val deselected  = Color.argb(255,255,255,255)
         val selected = Color.argb(255,9,135,109)
         val adjacent = Color.argb(255,0,0,0)
+        val turret = Color.argb(255,255,0,0)
     }
 }
