243dd36ce17e695da343025abdb6c216013b8350
0ldmanB <davidstephenbarry@gmail.com>
2018-09-30 16:27:25 -0700

    Random bed color can now be limited to certain ranges (eg, only orange hues), and multiple ranges can be stored and cycled through, (orange hues, then blue hues, then green hues, etc.)

--- app/src/main/java/com/example/android/zoomlayouttest/ColorData.kt
+++ app/src/main/java/com/example/android/zoomlayouttest/ColorData.kt
@@ -3,7 +3,7 @@ package com.example.android.zoomlayouttest
 import android.graphics.Color
 import java.lang.Math
 
-class ColorData{
+class ColorData {
 
     /**
      * If adding a new hardcoded color in color list, make sure to also add the color to the listOfColors list
@@ -20,6 +20,14 @@ class ColorData{
         val turret = Color.argb(255,255,0,0)
         val listOfColors = mutableListOf(deselected, selected, adjacent,turret)
         var nextBedColor : Int? = null
+
+        private var color1 = ColorRange(63,189,0,67,145,255)
+        private var color2 = ColorRange(206,255,100,150,0,33)
+        private var color3 = ColorRange(189,215,35,87,240,255)
+
+        private var listColorRange = mutableListOf<ColorRange>(color1,color2,color3)
+        private var colorRangeIndex = 0
+
         /**
          * random bed color generator -- simplified version, could be replaced with hardcoded colors
          */
@@ -27,22 +35,36 @@ class ColorData{
 
 
         fun newRandomBedColor(){
+
+
             var newColor : Int? = null
+            var fromRange = listColorRange[colorRangeIndex]
 
             while(newColor == null || listOfColors.contains(newColor)){
 
-                var a = (34..255).shuffled().first()
-                var r = (34..255).shuffled().first()
-                var g = (34..255).shuffled().first()
-                var b = (34..255).shuffled().first()
+
+
+                var a = 255
+                var r = (fromRange.rMin..fromRange.rMax).shuffled().first()
+                var g = (fromRange.gMin..fromRange.gMax).shuffled().first()
+                var b = (fromRange.bMin..fromRange.bMax).shuffled().first()
 
                 newColor = Color.argb(a,r,b,g)
 
             }
+
+            colorRangeIndex += 1
+
+            if(colorRangeIndex >= listColorRange.size){ colorRangeIndex = 0 }
+
             listOfColors.add(newColor)
             nextBedColor = newColor
 
+
         }
     }
+
+    data class ColorRange(val rMin:Int, val rMax :Int, val gMin : Int, val gMax : Int, val bMin : Int, val bMax : Int)
+
 
 }
