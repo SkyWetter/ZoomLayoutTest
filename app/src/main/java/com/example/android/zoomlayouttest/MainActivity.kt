6acc3770643068c37020d72997a917c8435bae06
0ldmanB <davidstephenbarry@gmail.com>
2018-10-02 19:01:06 -0700

    Finally got rid of det head weirdness.

--- app/src/main/java/com/example/android/zoomlayouttest/MainActivity.kt
+++ app/src/main/java/com/example/android/zoomlayouttest/MainActivity.kt
@@ -66,6 +66,7 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Toast
